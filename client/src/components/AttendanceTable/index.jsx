2e3058160389573f858880b933014c52a8675427
Keshav Agrawal <keshava.bca2022@ssism.org>
2024-06-05 11:53:33 +0530

    table updated

--- client/src/components/AttendanceTable/index.jsx
+++ client/src/components/AttendanceTable/index.jsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { useGetStudentsQuery } from "../../apis/service";
+import "./style.css";
 
 const AttendanceTable = () => {
   const { data } = useGetStudentsQuery();
@@ -7,28 +8,30 @@ const AttendanceTable = () => {
   return (
     <Container>
       <Row>
-        <h2 className="text-center fw-bold mb-4">Attendance Table</h2>
+        <h2 className="text-center fw-bold mb-4 mt-5">Attendance Table</h2>
       </Row>
       <Row className="d-flex justify-content-center align-items-center">
         <Col className="col-xl-6 col-lg-6 col-12 d-flex justify-content-center flex-column">
-          <table>
+          <table border={2} className="text-center">
             <thead>
               <tr>
+                <th>S.No</th>
                 <th>Name</th>
                 <th>Subject</th>
                 <th>Attendance Dates</th>
               </tr>
             </thead>
             <tbody>
-              {data?.students.map((student) => (
+              {data?.students.map((student, index) => (
                 <tr key={student._id}>
+                  <td>{index + 1}</td>
                   <td>{student.fullName}</td>
                   <td>{student.subject}</td>
                   <td>
                     {student.attendance.length > 0 ? (
                       student.attendance.map((attendance) => (
                         <span key={attendance._id}>
-                          {attendance.date.toLocaleDateString() + " "}
+                          {attendance.date.split("T")[0]}
                         </span>
                       ))
                     ) : (
